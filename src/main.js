f5898b485c43b3576b1dbbbf28958287d2ce760f
HazelHsieh <owo0726@gmail.com>
2023-02-17 11:45:25 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// scss
+import './assets/all.scss'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.use(router)
+app.mount('#app')
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,19 @@
+<template>
+  這是專屬後台的頁面
+    <router-link to="/back/products">後台產品列表</router-link> |
+    <router-link to="/back/orders">後台訂單列表</router-link> |
+    <router-link to="/">回前台首頁</router-link>
+
+  <hr>
+  <RouterView></RouterView>
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
